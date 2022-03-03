c3cfcf4032c2ad22adb3443855a312f75f89dc2f
성민 <kevin@seongmin-ui-MacBookAir.local>
2022-03-04 00:59:59 +0900

    first

---
+++ App.tsx
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useEffect} from 'react';
+import {Alert, SafeAreaView, StyleSheet} from 'react-native';
+import Config from 'react-native-config';
+
+const App = () => {
+  useEffect(() => {
+    console.log(Config.API_URL);
+  }, []);
+
+  return <SafeAreaView></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
